6a3d7f7969de96d1e72b6c0a0996106c00a61377
vovaent <vovaent@gmail.com>
2023-05-14 01:05:17 +0300

    Article fixed

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -95,8 +95,8 @@
                         <div class="ArticleBottomSocialShareButtons">
                             <a href="https://pinterest.com/pin/create/button/?url=<?php echo get_permalink(); ?>/<?php if (get_the_excerpt()) {
                                                                                                                         echo ('&description=' . get_the_excerpt());
-                                                                                                                    } ?>" class="ArticleBottomSocialShareButtonsItem">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                                                    } ?>" class="ArticleBottomSocialShareButtonsItem" target="_blank">
+                                <svg width=" 20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_4616_67332)">
                                         <path d="M8.885 12.55C8.48111 14.2635 8.08581 15.9146 7.22432 17.2167C6.96007 17.6156 6.69475 18.1177 6.2436 18.3333C5.57761 15.0073 6.97082 12.2083 7.5208 9.41249C6.81936 8.02394 7.25547 5.6729 8.75825 5.40624C10.9345 5.02082 10.3609 7.56665 9.99248 8.74999C9.78839 9.39999 9.42424 10.0219 9.47902 10.7344C9.6004 12.2354 11.5479 12.3333 12.5458 11.5594C13.9766 10.4542 14.3912 8.31769 14.2505 6.56249C14.0389 3.89061 10.9184 2.56245 8.16208 3.63016C6.74094 4.18019 5.45193 5.44582 5.26502 7.30519C5.16834 8.28436 5.37996 9.06665 5.7774 9.61874C5.83648 9.70311 6.04273 9.85103 6.07388 10.075C6.13618 10.5271 5.85904 11.0146 5.60554 11.3521C4.19406 10.9583 3.46577 9.7302 3.34976 8.17499C3.08117 4.63332 6.08032 1.94162 9.60685 1.68954C13.374 1.42183 16.3581 3.53015 16.6352 6.60415C16.8414 8.88853 16.0047 11.1875 14.677 12.426C13.678 13.3552 11.5565 14.2667 9.82169 13.3771C9.4382 13.1792 9.26633 12.9375 8.885 12.55Z" fill="#19C79D" />
                                     </g>
@@ -109,15 +109,15 @@
                             </a>
                             <a href="https://www.facebook.com/sharer?u=<?php echo get_permalink(); ?>;<?php if (get_the_excerpt()) {
                                                                                                             echo ('&t=' . get_the_excerpt());
-                                                                                                        } ?>" class="ArticleBottomSocialShareButtonsItem">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                                                        } ?>" class="ArticleBottomSocialShareButtonsItem" target="_blank">
+                                <svg width=" 20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M11.6663 11.25H13.7497L14.583 7.91666H11.6663V6.24999C11.6663 5.39166 11.6663 4.58332 13.333 4.58332H14.583V1.78332C14.3113 1.74749 13.2855 1.66666 12.2022 1.66666C9.93967 1.66666 8.33301 3.04749 8.33301 5.58332V7.91666H5.83301V11.25H8.33301V18.3333H11.6663V11.25Z" fill="#19C79D" />
                                 </svg>
                             </a>
                             <a href="http://twitter.com/intent/tweet<?php if (get_the_excerpt()) {
                                                                         echo ('?text=' . get_the_excerpt() . ';');
-                                                                    } ?>&url=<?php echo get_permalink(); ?>" class="ArticleBottomSocialShareButtonsItem">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    } ?>&url=<?php echo get_permalink(); ?>" class="ArticleBottomSocialShareButtonsItem" target="_blank">
+                                <svg width=" 24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_5047_74944)">
                                         <path d="M15.2995 5.54999C14.5395 5.54987 13.8098 5.84811 13.2674 6.38056C12.7251 6.913 12.4134 7.63709 12.3995 8.39699L12.3715 9.97199C12.3699 10.0566 12.3504 10.1398 12.3143 10.2163C12.2782 10.2928 12.2264 10.3608 12.1622 10.4159C12.098 10.4709 12.0228 10.5118 11.9417 10.5358C11.8606 10.5597 11.7754 10.5663 11.6915 10.555L10.1305 10.343C8.07653 10.063 6.10853 9.11699 4.22053 7.54399C3.62253 10.854 4.79053 13.147 7.60353 14.916L9.35053 16.014C9.43354 16.0662 9.5025 16.1379 9.55138 16.2228C9.60026 16.3078 9.62757 16.4035 9.63093 16.5015C9.63428 16.5995 9.61358 16.6968 9.57062 16.7849C9.52767 16.873 9.46378 16.9493 9.38453 17.007L7.79253 18.17C8.73953 18.229 9.63853 18.187 10.3845 18.039C15.1025 17.097 18.2395 13.547 18.2395 7.69099C18.2395 7.21299 17.2275 5.54999 15.2995 5.54999ZM10.3995 8.35999C10.417 7.39604 10.7184 6.45866 11.2661 5.66521C11.8138 4.87177 12.5833 4.25751 13.4784 3.89936C14.3736 3.54121 15.3544 3.45507 16.2983 3.65174C17.2421 3.84841 18.107 4.31914 18.7845 5.00499C19.4955 4.99999 20.1005 5.17999 21.4535 4.35999C21.1185 5.99999 20.9535 6.71199 20.2395 7.69099C20.2395 15.333 15.5425 19.049 10.7765 20C7.50853 20.652 2.75653 19.581 1.39453 18.159C2.08853 18.105 4.90853 17.802 6.53853 16.609C5.15953 15.7 -0.329469 12.47 3.27753 3.78599C4.97053 5.76299 6.68753 7.10899 8.42753 7.82299C9.58553 8.29799 9.86953 8.28799 10.4005 8.36099L10.3995 8.35999Z" fill="#19C79D" />
                                     </g>
@@ -194,9 +194,9 @@
                                 echo get_template_part('cards/card', 'article_medium');
                                 $i++;
                             endwhile;
-                            if ($i != 6) {
-                                echo get_template_part('cards/card', 'cta_medium', get_field('cta_content', 'option'));
-                            }
+                            // if ($i != 6) {
+                            //     echo get_template_part('cards/card', 'cta_medium', get_field('cta_content', 'option'));
+                            // }
                         } ?>
                     </div>
 
@@ -204,15 +204,15 @@
                         <?php if ($lastposts->have_posts()) {
                             $i = 1;
                             while ($lastposts->have_posts()) : $lastposts->the_post();
-                                if ($i == 6) {
+                                if ($i == 4) {
                                     echo get_template_part('cards/card', 'cta_medium', get_field('cta_content', 'option'));
                                 }
                                 echo get_template_part('cards/card', 'article_mini');
                                 $i++;
                             endwhile;
-                            if ($i != 6) {
-                                echo get_template_part('cards/card', 'cta_medium', get_field('cta_content', 'option'));
-                            }
+                            // if ($i != 6) {
+                            //     echo get_template_part('cards/card', 'cta_medium', get_field('cta_content', 'option'));
+                            // }
                         } ?>
                     </div>
 
@@ -241,9 +241,11 @@
 
         </div>
         <div class="ArticleSidebarButton">
-            <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 1L2 6L7 11" stroke="white" stroke-width="2" />
-            </svg>
+            <div class="ArticleSidebarButtonIcon">
+                <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 1L2 6L7 11" stroke="white" stroke-width="2" />
+                </svg>
+            </div>
         </div>
     </div>
 
